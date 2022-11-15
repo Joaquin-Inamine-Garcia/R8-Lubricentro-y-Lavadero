74a1726d74349a6a0e9e87e15920aa612d0cc623
Joaquin Inamine Garcia <joaquingarcia279@gmail.com>
2022-11-14 22:55:47 -0300

    14/11

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 1000 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 1000 });
+ScrollReveal().reveal('.two-cards', { delay: 1000 });
